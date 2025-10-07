1b4c9fac82bc8a42f0bde7d7ef4639f35efce6f7
Louis Remes <louisremes@hotmail.be>
2025-10-07 22:34:11 +0200

    added metrics and reset

--- main.go
+++ main.go
@@ -4,20 +4,51 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	mux := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	_, err := w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+	if err != nil {
+		fmt.Println("failed to write the response body: %w", err)
+	}
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	_, err := w.Write([]byte("Reset hits"))
+	if err != nil {
+		fmt.Println("failed to write the response body: %w", err)
+	}
+}
+
+func main() {
+	apiCfg := apiConfig{}
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte("OK"))
 		if err != nil {
 			fmt.Println("failed to write the response body: %w", err)
 		}
 	})
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	svr := &http.Server{
 		Handler: mux,
